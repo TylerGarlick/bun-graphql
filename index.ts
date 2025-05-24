2a0a4f91719ef66ed446a04b4a5ff322d03b185b
Tyler Garlick <tyler-the-robot@outlook.com>
2025-05-23 23:46:09 -0600

    :tada: Additions to the standard template.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { createSchema, createYoga } from 'graphql-yoga'
 import { createServer } from 'node:http'
 import { typeDefs as scalarTypeDefs } from 'graphql-scalars'
 
-const { PORT: port = `3000` } = Bun.env
+const { PORT: port = `4000` } = Bun.env
 
 const PORT = parseInt(port)
 
@@ -22,12 +22,24 @@ export const schema = createSchema({
   },
 })
 
-const yoga = createYoga({ schema, landingPage: true, graphiql: true })
-
-// Pass it into a server to hook into request handlers.
-const server = createServer(yoga)
-
-// Start the server and you're done!
-server.listen(PORT, () => {
-  console.info(`Server is running on http://localhost:${PORT}/graphql`)
+const yoga = createYoga({
+  schema,
+  landingPage: true,
+  graphiql: true,
+  graphqlEndpoint: '/',
 })
+
+Bun.serve({
+  fetch: yoga,
+  port: PORT,
+})
+
+console.info(`Server is running on http://localhost:${PORT}`)
+
+// // Pass it into a server to hook into request handlers.
+// const server = createServer(yoga)
+//
+// // Start the server and you're done!
+// server.listen(PORT, () => {
+//
+// })
